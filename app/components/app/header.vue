7c2b1ef3db068d8c7aadc5d955275c3593d13c6d
nasnuttapum <nas.nuttapum@gmail.com>
2025-08-19 12:07:45 +0700

    first commit

---
+++ app/components/app/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="font-[sans-serif] Bg-gray-900 min-h-screen">
+        <!-- Start Header-->
+        <div class=" text-gray-900 bg-[#C97D7D]">
+            <h1>Header</h1>
+        </nav> class="container px-6 py-[0.8rem] mx-auto md:justify-between md:item-center shadow-x1 pl-5 md:px-30 z-0 cursor"
+        </div>
+        <!--End Header -->
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <app-header />
+        <slot />
+        <AppFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { AppFooter } from '#components';
+
+
+</script>
+
+<style scoped></style>
